3f62eae9612992f07819840c3bc95bdadf2e6da3
metalkick <imsaad.t@gmail.com>
2022-02-23 02:35:25 -0500

    initial web app skeleton

---
+++ web_app/src/routes.js
@@ -0,0 +1,57 @@
+// @mui material components
+import Icon from "@mui/material/Icon";
+
+// Pages
+import AboutUs from "layouts/pages/landing-pages/about-us";
+import ContactUs from "layouts/pages/landing-pages/contact-us";
+import Author from "layouts/pages/landing-pages/author";
+import SignIn from "layouts/pages/authentication/sign-in";
+import SignUp from "layouts/pages/authentication/sign-up";
+
+const routes = [
+  {
+    name: "pages",
+    icon: <Icon>dashboard</Icon>,
+    columns: 1,
+    rowsPerColumn: 2,
+    collapse: [
+      {
+        name: "landing pages",
+        collapse: [
+          {
+            name: "about us",
+            route: "/pages/landing-pages/about-us",
+            component: <AboutUs />,
+          },
+          {
+            name: "contact us",
+            route: "/pages/landing-pages/contact-us",
+            component: <ContactUs />,
+          },
+          {
+            name: "author",
+            route: "/pages/landing-pages/author",
+            component: <Author />,
+          },
+        ],
+      },
+      {
+        name: "account",
+        collapse: [
+          {
+            name: "sign in",
+            route: "/pages/authentication/sign-in",
+            component: <SignIn />,
+          },
+          {
+            name: "sign up",
+            route: "/pages/authentication/sign-up",
+            component: <SignUp />,
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default routes;
